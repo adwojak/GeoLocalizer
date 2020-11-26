28a3548188f885dfdd2fde75554aa3f57141761d
Artur Dwojak <dwojaka207@gmail.com>
2020-11-27 00:23:38 +0100

    Added creating .env to createapp.sh

--- createapp.sh
+++ createapp.sh
@@ -5,3 +5,6 @@ source "./venv/bin/activate"
 pip install -r requirements
 python manage.py makemigrations geolocalizer
 python manage.py migrate
+touch geolocalizer/.env
+echo IPSTACK_API_KEY= >> geolocalizer/.env
+echo DJANGO_SECRET_KEY= >> geolocalizer/.env
